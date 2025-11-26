f0f3eb537831f9b9270fb81fd9f85527760cc868
DeepanshuDahiya <deepanshu31232@gmail.com>
2025-11-26 18:35:58 +0530

    Remove bugs

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import SuggestionProvider from "./context/SuggestionContext.jsx";
-import CartProvider from "./context/CartProvider.jsx";
+import CartProvider from "./context/CartContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
